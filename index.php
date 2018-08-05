20c5a06df1fe3a0cf3eed1334240be52d3bd671b
Sandalanka <1415sandalanka@gmail.com>
2018-08-05 23:15:48 +0530

    firs commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<head>
+<link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/960.css" />
+<link rel="stylesheet" type="text/css" media="all" href="css/style.css" />
+<link href="galleria.css" rel="stylesheet" type="text/css" media="screen"> 
+<script type="text/javascript" src="jquery-1.3.min.js"></script> 
+<script type="text/javascript" src="jquery.galleria.js"></script> 
+<script type="text/javascript"> 
+jQuery(function($) { $('ul.gallery').galleria(); }); 
+</script>
+</head>
+
+<title>simpleGallery by Bryant Smith</title>
+</head>
+
+<body>
+<div class="container_12">
+<div class="grid_4">
+		<h1>300px</h1>
+        <h2>The Gallery</h2>
+	</div>
+	<!-- end .grid_4 -->
+	<div class="grid_8">
+		<p>
+			
+            
+        <ul class="gallery"> 
+			<li><img src="./images/1.jpg" title="An HDR Field" alt="1"></li> 
+            <li><img src="./images/2.jpg" title="Nice Sky HDR" alt="2"></li> 
+            <li><img src="./images/3.jpg" title="Cool Colors" alt="3"></li> 
+            <li><img src="./images/4.jpg" title="Indoors" alt="4"></li> 
+		</ul>
+            
+            
+            
+            
+		</p>
+	</div>
+	<!-- end .grid_8 -->
+</div>
+
+</body>
+</html>
